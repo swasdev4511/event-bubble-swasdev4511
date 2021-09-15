b01541bb979ca20d421b044229dfeb4dc85a1932
swasdev4511 <swastiksupadhye@gmail.com>
2021-09-15 13:23:45 +0530

    2 files modified

--- index.js
+++ index.js
@@ -2,8 +2,54 @@
 import './style.css';
 
 // Write Javascript code!
-function clickHandler(event) {
-  console.log(event);
-}
 
-window.addEventListener('click', clickHandler, false);
+const grandParent = document.querySelector('#grandParent');
+const parent = document.querySelector('#parent');
+const child = document.querySelector('#child');
+
+/* EVENT - BUBBLING */
+grandParent.addEventListener(
+  'click',
+  e => {
+    console.log(' I am grand Parent!');
+  },
+  false
+);
+parent.addEventListener(
+  'click',
+  e => {
+    console.log(' I am Parent!');
+  },
+  false
+);
+child.addEventListener(
+  'click',
+  e => {
+    console.log(' I am Child!');
+  },
+  false
+);
+
+/* EVENT - CAPTURING */
+// grandParent.addEventListener(
+//   'click',
+//   e => {
+//     console.log(' I am grand Parent!');
+//   },
+//   true
+// );
+// parent.addEventListener(
+//   'click',
+//   e => {
+//     console.log(' I am Parent!');
+//   },
+//   true
+// );
+// child.addEventListener(
+//   'click',
+//   e => {
+//     console.log(' I am Child!');
+//     e.stopPropagation();
+//   },
+//   true
+// );
